dec0b3534456be2a413b3d0e3f9dd63050a87e91
sivahawking <40987841+sivahawking@users.noreply.github.com>
2021-03-20 11:26:46 +0530

    exceptions example

--- exceptions.py
+++ exceptions.py
@@ -4,3 +4,20 @@ while True:
         break
     except ValueError:
         print("Oops! That was no valid number. Try again...")
+#------------------------------------------------------------------------------------
+try:
+    raise KeyboardInterrupt
+finally:
+    print('Goodbye, world!')
+#-------------------------------------------------------------------------------------
+def divide(x, y):
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        print("division by zero!")
+    else:
+        print("result is", result)
+    finally:
+        print("executing finally clause")
+
+print(divide(2, 4))
